19b494cdafb32ac74e284642288b58dac7badc28
khageshjayaswal <60471683+khageshjayaswal@users.noreply.github.com>
2022-06-09 11:57:28 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System; // Namespace for Console output
+using System.Configuration; // Namespace for ConfigurationManager
+using System.Threading.Tasks; // Namespace for Task
+using Azure.Identity;
+using Azure.Storage.Queues; // Namespace for Queue storage types
+using Azure.Storage.Queues.Models;// Namespace for PeekedMessage
+
+namespace storagequeue8_6_22
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+            //QueueClient queue = new QueueClient(connectionString, "mystoragequeue");
+            string queue = "mystoragequeue";
+            string msg = "jully";
+            //InsertMessage(queue, msg);
+            //PeekMessage(queue);
+            //DequeueMessage(queue);
+            GetQueueLength(queue);
+        }
+
+        public static void InsertMessage(string queueName, string message)
+        {
+            // Get the connection string from app settings
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            // Instantiate a QueueClient which will be used to create and manipulate the queue
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            // Create the queue if it doesn't already exist
+            queueClient.CreateIfNotExists();
+
+            if (queueClient.Exists())
+            {
+                // Send a message to the queue
+                queueClient.SendMessage(message);
+            }
+
+            Console.WriteLine($"Inserted: {message}");
+        }
+
+        private static void PeekMessage(string queueName)
+        {
+            // Get the connection string from app settings
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            // Instantiate a QueueClient which will be used to manipulate the queue
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            if (queueClient.Exists())
+            {
+                // Peek at the next message
+                PeekedMessage[] peekedMessage = queueClient.PeekMessages();
+
+                // Display the message
+                Console.WriteLine($"Peeked message: '{peekedMessage[0].Body}'");
+                Console.ReadKey();
+            }
+        }
+
+        public static void DequeueMessage(string queueName)
+        {
+            // Get the connection string from app settings
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            // Instantiate a QueueClient which will be used to manipulate the queue
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            if (queueClient.Exists())
+            {
+                // Get the next message
+                QueueMessage[] retrievedMessage = queueClient.ReceiveMessages();
+
+                // Process (i.e. print) the message in less than 30 seconds
+                Console.WriteLine($"Dequeued message: '{retrievedMessage[0].Body}'");
+                Console.ReadKey();
+                // Delete the message
+                queueClient.DeleteMessage(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+            }
+        }
+
+        public static void GetQueueLength(string queueName)
+        {
+            // Get the connection string from app settings
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            // Instantiate a QueueClient which will be used to manipulate the queue
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            if (queueClient.Exists())
+            {
+                QueueProperties properties = queueClient.GetProperties();
+
+                // Retrieve the cached approximate message count.
+                int cachedMessagesCount = properties.ApproximateMessagesCount;
+
+                // Display number of messages.
+                Console.WriteLine($"Number of messages in queue: {cachedMessagesCount}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
